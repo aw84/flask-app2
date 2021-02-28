d8a99ae9d589f8f669fc757b363f7c955725134e
aw84 <silencewalker@gmail.com>
2021-02-28 21:36:27 +0100

    Migrate init

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,17 @@ from flask_migrate import Migrate
 from app.config import get_config_by_name
 from app.model import db
 
+migrate = Migrate()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(get_config_by_name(config_name))
 
     db.init_app(app)
+    migrate.init_app(app)
 
     return app
 
 
 app = create_app('DEV')
-migrate = Migrate(app, db)
+
