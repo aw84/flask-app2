1a3a978febab6c2c3993f939b9fbad3e38bd62c0
aw84 <silencewalker@gmail.com>
2021-03-05 08:20:05 +0100

    update

--- app/service/Auth.py
+++ app/service/Auth.py
@@ -3,7 +3,21 @@ from flask import Blueprint, request, jsonify
 auth_bp = Blueprint('auth', __name__)
 
 
+def sanitaze_user_input(data):
+    # TODO: encoding, sanitaze, escape,...
+    return data
+
+
 @auth_bp.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-    return jsonify(data), 200
+    
+    user_name = sanitaze_user_input(data['user'])
+    user_pass = sanitaze_user_input(data['pass'])
+    
+    
+
+    return jsonify({
+        'user': user_name,
+        'pass': user_pass
+    }), 200
